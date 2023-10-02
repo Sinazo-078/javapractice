f1d8b1c216bfe770a9b2de52171d9ddafb3bbb17
Sinazo-078 <sihentshesinazo44@gmail.com>
2023-10-02 09:26:07 +0200

    First commit....

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[]args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("what is your name");
+		String name= scanner.nextLine();
+		System.out.println("hello"+name);
+		System.out.println("how old are you");
+		int age= scanner. nextInt();
+		System.out.println("you are "+age+" years old");
+		
+		
+	}
+	
+}
+
+
+
+	
