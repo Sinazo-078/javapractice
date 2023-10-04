0df9585ae623a24293c8ad6c7959d4ff8fac91ac
Sinazo-078 <sihentshesinazo442gmail.com>
2023-10-04 09:40:43 +0200

    graphical user

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main{
 	public static void main(String[]args) {
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("what is your name");
-		String name= scanner.nextLine();
-		System.out.println("hello"+name);
-		System.out.println("how old are you");
-		int age= scanner. nextInt();
-		System.out.println("you are "+age+" years old");
+		
+
+	        String name = JOptionPane.showInputDialog("Enter your name");
+	        JOptionPane.showMessageDialog(null, "Hello " + name);
 		
 		
 	}
