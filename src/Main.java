1deb2efa7d2f8bcaba704eab1d171da61dcaaa16
Sinazo-078 <sihentshesinazo442gmail.com>
2023-10-04 09:54:38 +0200

    latest changes

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main{
 
 	        String name = JOptionPane.showInputDialog("Enter your name");
 	        JOptionPane.showMessageDialog(null, "Hello " + name);
+	         
+	        int Age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+	        JOptionPane.showMessageDialog(null, "your are " + Age + " years old" );
+	        
+	        
+	        
 		
 		
 	}
